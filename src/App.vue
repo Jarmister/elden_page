6171db901491674b9b6e69bf612881f84d3715a9
Jarosław Kotowski <jarmister3000@gmail.com>
2024-05-06 01:12:31 +0200

    CZY DAMY RADE TAK DAMY RADE

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- 
+Main Page
+MAP PAGE 
+SECONDARY Info  
+ 1 sidebar - with information on elden ring character 
+ 2 loader - loading effect simmilar to elden ring intro or menu screen
+ 3 family tree
+
+
+
+ --> <script setup>
+ import { useQuery } from '@vue/apollo-composable'
+ import gql from 'graphql-tag'
+
+ const { result } = useQuery(gql`
+  query {
+    boss(name: "Malenia, Blade Of Miquella") {
+    id,
+    name,
+    description,
+    image
+  }
+}
+ `)
+ </script>
+
+ <template>
+   <h2>Hello {{ result ?? 'world' }}</h2>
+ </template>
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+
+const httpLink = createHttpLink({
+    uri: 'https://eldenring.fanapis.com/api/graphql',
+  })
+  const cache = new InMemoryCache()
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+
+createApp(App)
+.provide(DefaultApolloClient, apolloClient)
+.mount('#app')
